3cd15d1526c8449259393ea2cba413e21ab87f9e
sxclij <sxclij@gmail.com>
2024-12-14 13:36:39 +0900

    icon size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -160,36 +160,41 @@
 </main>
 <style>
     :root {
-        --primary-text-color: #e0e0e0;
-        --secondary-text-color: #bdbdbd;
-        --accent-color: #81c784;
-        --link-color: #64b5f6;
-        --background-color: #121212;
-        --card-background-color: #1e1e1e;
-        --item-background-color: #282828;
-        --font-size-base: 1rem;
-        --font-size-h2: 1.8rem;
-        --font-size-h3: 1.125rem;
+      --primary-text-color: #e0e0e0;
+      --secondary-text-color: #bdbdbd;
+      --accent-color: #81c784;
+      --link-color: #64b5f6;
+      --background-color: #121212;
+      --card-background-color: #1e1e1e;
+      --item-background-color: #282828;
+      --font-size-base: 1rem;
+      --font-size-h2: 1.8rem;
+      --font-size-h3: 1.125rem;
     }
+  
     :global(body) {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-        line-height: 1.6;
-        background-color: var(--background-color);
-        color: var(--primary-text-color);
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      line-height: 1.6;
+      background-color: var(--background-color);
+      color: var(--primary-text-color);
     }
+  
     :global(a) {
-        color: var(--link-color);
-        text-decoration: none;
-        font-weight: bold;
+      color: var(--link-color);
+      text-decoration: none;
+      font-weight: bold;
     }
+  
     :global(a:hover) {
-        text-decoration: underline;
+      text-decoration: underline;
     }
+  
     :global(a:visited) {
         color: var(--accent-color);
     }
+  
     .navbar {
         position: fixed;
         top: 0;
@@ -200,156 +205,162 @@
         box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
         z-index: 1000;
     }
+  
     .navbar ul {
-        list-style: none;
-        display: flex;
-        justify-content: center;
-        margin: 0;
-        padding: 0;
+      list-style: none;
+      display: flex;
+      justify-content: center;
+      margin: 0;
+      padding: 0;
     }
+  
     .navbar li {
-        margin: 0 1rem;
+      margin: 0 1rem;
     }
+  
     .navbar a {
-        color: var(--primary-text-color);
-        font-size: var(--font-size-base);
-        transition: color 0.3s;
+      color: var(--primary-text-color);
+      font-size: var(--font-size-base);
+      transition: color 0.3s;
     }
+  
     .navbar a:hover {
-        color: var(--accent-color);
+      color: var(--accent-color);
     }
+  
     main {
-        max-width: 85%;
-        margin: 4rem auto 0;
-        padding: 2rem 1rem;
-        background: var(--card-background-color);
-        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
-        border-radius: 8px;
+      max-width: 85%;
+      margin: 4rem auto 0;
+      padding: 2rem 1rem;
+      background: var(--card-background-color);
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
+      border-radius: 8px;
     }
     .hero {
-        display: flex;
-        align-items: flex-start;
-        margin-bottom: 2rem;
-        padding: 1rem;
+      display: flex;
+      align-items: flex-start;
+      margin-bottom: 2rem;
+      padding: 1rem;
     }
     .hero-icon-container {
-        flex: 0 0 auto;
-        margin-right: 2rem;
+      flex: 0 0 auto;
+      margin-right: 2rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 200px; /* ここで幅を調整 */
+    }
+    .hero-content-container {
+      flex: 1;
+      text-align: left;
+    }
+    .profile-icon {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+    .hero h1 {
+      margin: 0;
+      font-size: 2.5rem;
+      color: var(--accent-color);
+    }
+    .hero p {
+      margin: 0.5rem 0 0;
+      font-size: 1.2rem;
+      color: var(--secondary-text-color);
+    }
+    .info-list {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+      gap: 1rem;
+      max-width: 100%;
+    }
+    .info-item {
+      background-color: var(--item-background-color);
+      padding: 0.5rem;
+      border-radius: 4px;
+      box-sizing: border-box;
+      min-width: 0;
+    }
+    .info-text {
+      display: flex;
+      flex-direction: column;
+    }
+    .info-item h2 {
+      font-size: 1.1rem;
+      font-weight: bold;
+      margin: 0 0 0.2rem 0;
+    }
+  
+    .section {
+      margin-bottom: 2rem;
+    }
+  
+    .section h2 {
+      font-size: var(--font-size-h2);
+      border-bottom: 2px solid var(--accent-color);
+      padding-bottom: 0.3rem;
+      margin-bottom: 1rem;
+    }
+  
+    .section p {
+      margin-bottom: 0.5rem;
+    }
+  
+    .skill-list,
+    .project-list {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 1rem;
+    }
+  
+    .skill-item h3,
+    .project-item h3 {
+        border-left-width: 6px;
+        border-left-style: solid;
+        padding-left: 1rem;
+        font-size: var(--font-size-h3);
+    }
+  
+    .link-list li {
+      margin-bottom: 0.5rem;
+    }
+    .footer {
+      text-align: center;
+      margin-top: 2rem;
+      font-size: 0.9rem;
+      color: #e7e7e7;
+    }
+    @media (max-width: 768px) {
+      .navbar {
+        display: none;
+      }
+      .hero {
+        flex-direction: column;
+      }
+      .hero-icon-container {
+        margin-right: 0;
+        margin-bottom: 1rem;
+        width: 100%;
         display: flex;
         align-items: center;
         justify-content: center;
-    }
-    .hero-content-container {
-        flex: 1;
-        text-align: left;
-    }
-    .profile-icon {
-        width: 50%;
-        height: 50%;
-        border-radius: 50%;
-        object-fit: cover;
-    }
-    .hero h1 {
-        margin: 0;
-        font-size: 2.5rem;
-        color: var(--accent-color);
-    }
-    .hero p {
-        margin: 0.5rem 0 0;
-        font-size: 1.2rem;
-        color: var(--secondary-text-color);
-    }
-    .info-list {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-        gap: 1rem;
+      }
+       .hero-content-container {
+          width: 100%;
+       }
+      .profile-icon {
+        width: 30%;
+        height: 30%;
+      }
+      .info-list,
+      .skill-list,
+      .project-list {
+        grid-template-columns: 1fr;
+      }
+      .info-item {
         max-width: 100%;
+      }
     }
-    .info-item {
-        background-color: var(--item-background-color);
-        padding: 0.5rem;
-        border-radius: 4px;
-        box-sizing: border-box;
-    }
-    .info-text {
-        display: flex;
-        flex-direction: column;
-    }
-    .info-item h2 {
-        font-size: 1.1rem;
-        font-weight: bold;
-        margin: 0 0 0.2rem 0;
-    }
-    .section {
-        margin-bottom: 2rem;
-    }
-    .section h2 {
-        font-size: var(--font-size-h2);
-        border-bottom: 2px solid var(--accent-color);
-        padding-bottom: 0.3rem;
-        margin-bottom: 1rem;
-    }
-    .section p {
-        margin-bottom: 0.5rem;
-    }
-    .skill-list {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap: 1rem;
-    }
-    .skill-item h3 {
-        border-left: 6px solid;
-        padding-left: 1rem;
-        font-size: var(--font-size-h3);
-    }
-    .project-list {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap: 1rem;
-    }
-    .project-item h3 {
-        border-left: 6px solid;
-        padding-left: 1rem;
-        font-size: var(--font-size-h3);
-    }
-    .link-list li {
-        margin-bottom: 0.5rem;
-    }
-    .footer {
-        text-align: center;
-        margin-top: 2rem;
-        font-size: 0.9rem;
-        color: #e7e7e7;
-    }
-    @media (max-width: 768px) {
-        .navbar {
-            display: none;
-        }
-        .hero {
-            flex-direction: column;
-        }
-        .hero-icon-container {
-            margin-right: 0;
-            margin-bottom: 1rem;
-            width: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-        .hero-content-container {
-            width: 100%;
-        }
-        .profile-icon {
-            width: 30%;
-            height: 30%;
-        }
-        .info-list,
-        .skill-list,
-        .project-list {
-            grid-template-columns: 1fr;
-        }
-        .info-item {
-            max-width: 100%;
-        }
-    }
-</style>
+  </style>
