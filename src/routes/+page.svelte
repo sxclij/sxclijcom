0286269689312b362f1b466d8541960065739524
sxclij <sxclij@gmail.com>
2024-12-31 17:49:37 +0900

    layout global

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -161,37 +161,6 @@
 </main>
 
 <style>
-  :root {
-    --primary-text-color: #e0e0e0;
-    --secondary-text-color: #bdbdbd;
-    --accent-color: #81c784;
-    --link-color: #64b5f6;
-    --background-color: #121212;
-    --card-background-color: #1e1e1e;
-    --item-background-color: #282828;
-    --font-size-base: 1rem;
-    --font-size-h2: 1.8rem;
-    --font-size-h3: 1.125rem;
-  }
-  :global(body) {
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 0;
-    line-height: 1.6;
-    background-color: var(--background-color);
-    color: var(--primary-text-color);
-  }
-  :global(a) {
-    color: var(--link-color);
-    text-decoration: none;
-    font-weight: bold;
-  }
-  :global(a:hover) {
-    text-decoration: underline;
-  }
-  :global(a:visited) {
-    color: var(--accent-color);
-  }
   main {
     max-width: 85%;
     margin: 4rem auto 0;
