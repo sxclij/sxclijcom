fc9a6757b7edef3e3018a37f226ca83dbc3708b7
sxclij <sxclij@gmail.com>
2024-12-31 19:35:35 +0900

    portal

---
+++ src/routes/daily/20250101/+page.svelte
@@ -0,0 +1,14 @@
+<main>
+  <h1>2025/01/01の日報</h1>
+</main>
+
+<style>
+  main {
+    max-width: 85%;
+    margin: 4rem auto 0;
+    padding: 2rem 1rem;
+    background: var(--card-background-color);
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
+    border-radius: 8px;
+  }
+</style>
---
+++ src/routes/portal/+page.svelte
@@ -0,0 +1,15 @@
+<main>
+  <h1>ポータル</h1>
+  sxclij.com内の全てのページを検索できるようにする予定。
+</main>
+
+<style>
+  main {
+    max-width: 85%;
+    margin: 4rem auto 0;
+    padding: 2rem 1rem;
+    background: var(--card-background-color);
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
+    border-radius: 8px;
+  }
+</style>
