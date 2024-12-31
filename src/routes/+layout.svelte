41afae31b6267e918a3a160da2cb4c4d0458aafa
sxclij <sxclij@gmail.com>
2024-12-31 17:44:42 +0900

    layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<nav class="navbar">
+    <ul>
+        <li><a href="/">sxclij.com</a></li>
+        <li><a href="/daily">日報</a></li>
+    </ul>
+</nav>
+<slot></slot>
+
+<style>
+    .navbar {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        background: var(--card-background-color);
+        padding: 0.5rem 1rem;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+        z-index: 1000;
+    }
+    .navbar ul {
+        list-style: none;
+        display: flex;
+        justify-content: center;
+        margin: 0;
+        padding: 0;
+    }
+    .navbar li {
+        margin: 0 1rem;
+    }
+    .navbar a {
+        color: var(--primary-text-color);
+        font-size: var(--font-size-base);
+        transition: color 0.3s;
+    }
+    .navbar a:hover {
+        color: var(--accent-color);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -94,12 +94,6 @@
   <meta name="description" content="sxclijさんのお手製サイト" />
 </svelte:head>
 
-<nav class="navbar">
-  <ul>
-    <li><a href="/">sxclij.com</a></li>
-    <li><a href="/daily">日報</a></li>
-  </ul>
-</nav>
 <main>
   <header class="hero">
     <div class="hero-icon-container">
@@ -198,34 +192,6 @@
   :global(a:visited) {
     color: var(--accent-color);
   }
-  .navbar {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    background: var(--card-background-color);
-    padding: 0.5rem 1rem;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
-    z-index: 1000;
-  }
-  .navbar ul {
-    list-style: none;
-    display: flex;
-    justify-content: center;
-    margin: 0;
-    padding: 0;
-  }
-  .navbar li {
-    margin: 0 1rem;
-  }
-  .navbar a {
-    color: var(--primary-text-color);
-    font-size: var(--font-size-base);
-    transition: color 0.3s;
-  }
-  .navbar a:hover {
-    color: var(--accent-color);
-  }
   main {
     max-width: 85%;
     margin: 4rem auto 0;
@@ -320,9 +286,6 @@
     color: #e7e7e7;
   }
   @media (max-width: 768px) {
-    .navbar {
-      display: none;
-    }
     .hero {
       flex-direction: column;
     }
