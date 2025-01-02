a85636b21c12b3315dd4185d3bfec028826a9df3
sxclij <sxclij@gmail.com>
2025-01-02 23:49:36 +0900

    。

--- src/routes/daily/20241231/+page.svelte
+++ src/routes/daily/20241231/+page.svelte
@@ -11,14 +11,3 @@
   レイアウトの処理で若干苦戦しているが、後の私がなんとかしてくれると思う。<br />
   文章が本当に下手でどうにもならない気もするけど、まあたくさん書けば改善されるでしょということで（希望的観測）
 </main>
-
-<style>
-  main {
-    max-width: 85%;
-    margin: 4rem auto 0;
-    padding: 2rem 1rem;
-    background: var(--card-background-color);
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
-    border-radius: 8px;
-  }
-</style>
--- src/routes/daily/20250101/+page.svelte
+++ src/routes/daily/20250101/+page.svelte
@@ -11,14 +11,3 @@
   <h3>ホームページ</h3>
   sxclij.comの検索ページを作りたい。
 </main>
-
-<style>
-  main {
-    max-width: 85%;
-    margin: 4rem auto 0;
-    padding: 2rem 1rem;
-    background: var(--card-background-color);
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
-    border-radius: 8px;
-  }
-</style>
